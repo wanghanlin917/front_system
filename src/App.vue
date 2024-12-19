f80c85203d67e9a6e3afd9fd6fc527c22a90212e
wanghanlin <wanghanlin917@163.com>
2024-12-20 00:15:49 +0800

    2024.12.20 home

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-    <router-view></router-view>
+  <router-view>
+    <el-config-provider :locale="zhCn">
+      <router-view />
+    </el-config-provider>
+  </router-view>
 </template>
 
-
 <script setup>
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </script>
 
-
 <style>
-body{
-    @apply bg-gray-100;
+body {
+  @apply bg-gray-100;
 }
-#nprogress .bar{
+#nprogress .bar {
   background-color: #f4f4f4 !important;
   height: 3px !important;
 }
-
 </style>
--- src/views/admin/UserView.vue
+++ src/views/admin/UserView.vue
@@ -1,8 +1,16 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 const employeeList = ref([])
 
-employeeList.value = [{'name':'jk','username':'jk','phonenumber':'12520332','roleName':'CEO','createTime':'2024.12.19'}]
+employeeList.value = [
+  {
+    name: 'jk',
+    username: 'jk',
+    phoneNumber: '12520332666',
+    roleName: 'CEO',
+    createTime: '2024.12.19'
+  }
+]
 
 const params = ref({
   page: 1,
@@ -10,85 +18,138 @@ const params = ref({
   name: ''
 })
 
+const searchData = ref({ username: '' })
+
 const total = ref(10)
-
-
 </script>
 <template>
   <div class="employee-container">
-        <!-- 搜索区域 -->
-        <div class="search-container">
-      <div class="search-label">员工姓名：</div>
-      <el-input clearable placeholder="请输入内容" class="search-main" />
-      <el-button type="primary">查询</el-button>
-    </div>
-    <div class="create-container">
-      <el-button type="primary" @click="addEmployee">添加员工</el-button>
-    </div>
-    <div class="table">
-      <el-table style="width: 100%" :data="employeeList" >
-        <el-table-column type="index" label="序号" :index="indexMethod" width='100px' />
-        <el-table-column label="员工姓名" width="180" prop="name" />
-        <el-table-column label="登录账号" width="180" prop="username" />
-        <el-table-column label="联系方式" width="120" prop="phonenumber" />
-        <el-table-column label="角色" width="120" prop="roleName" />
-        <el-table-column label="添加时间" prop="createTime" width="100px" />
-        <el-table-column label="操作" fixed="right" width="200">
-          <template #default="scope">
-            <el-button size="mini" type="text">编辑</el-button>
-            <el-button
-              size="mini"
-              type="text"
-              @click="delEmployee(scope.row.id)"
-              >删除</el-button
-            >
-            <el-button size="mini" type="text">重置密码</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
-    <div class="page-container">
-      <el-pagination
-        layout="prev, pager, next"
-        :page-size="params.pageSize"
-        :total="total"
-        @current-change="pageChange"
-      />
-    </div>
+    <!-- 搜索区域 -->
+    <el-card v-loading="loading" shadow="never" class="search-wrapper">
+      <el-form ref="searchFormRef" :inline="true" :model="searchData">
+        <el-form-item prop="username" label="用户名">
+          <el-input v-model="searchData.username" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" :icon="Search" @click="handleSearch"
+            >查询</el-button
+          >
+          <el-button :icon="Refresh" @click="resetSearch">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card v-loading="loading" shadow="never">
+      <div class="create-container">
+        <el-button type="primary" @click="addEmployee">添加员工</el-button>
+      </div>
+      <div class="table">
+        <el-table :data="employeeList">
+          <el-table-column
+            type="index"
+            label="序号"
+            :index="indexMethod"
+            width="100"
+            align="center"
+          />
+          <el-table-column
+            label="员工姓名"
+            prop="name"
+            width="200"
+            align="center"
+          />
+          <el-table-column
+            label="登录账号"
+            prop="username"
+            width="200"
+            align="center"
+          />
+          <el-table-column
+            label="联系方式"
+            prop="phoneNumber"
+            width="300px"
+            align="center"
+          />
+          <el-table-column
+            label="角色"
+            width="200"
+            prop="roleName"
+            align="center"
+          />
+          <el-table-column
+            label="创建时间"
+            prop="createTime"
+            width="400px"
+            align="center"
+          />
+          <el-table-column
+            label="操作"
+            fixed="right"
+            width="200"
+            align="center"
+          >
+            <template #default="scope">
+              <el-button size="mini" type="text">编辑</el-button>
+              <el-button
+                size="mini"
+                type="text"
+                @click="delEmployee(scope.row.id)"
+                >删除</el-button
+              >
+              <el-button size="mini" type="text">重置密码</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
 
-    </div>
+      <div class="page-container">
+        <!-- <template> -->
+        <el-pagination
+          layout="total,prev, pager, next"
+          :page-size="params.pageSize"
+          :current-page="params.page"
+          :total="total"
+          @current-change="pageChange"
+        >
+          <template #total> 共 {{ total }} 条 </template>
+        </el-pagination>
+        <!-- </template> -->
+      </div>
+    </el-card>
+  </div>
 </template>
 
-
-
 <style lang="scss" scoped>
-.employee-container {
-  padding: 10px;
-}
+// .employee-container {
+//   // display: flex;
+//   // justify-content: space-between;
+//   // margin-bottom: 20px;
+//   padding: 10px;
+// }
 
-.search-container {
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid rgb(237, 237, 237, 0.9);
-  padding-bottom: 20px;
-
-  .search-label {
-    width: 100px;
-  }
-
-  .search-main {
-    width: 220px;
-    margin-right: 10px;
+.search-wrapper {
+  margin-bottom: 20px;
+  :deep(.el-card__body) {
+    padding-bottom: 2px;
   }
 }
-.create-container {
-  margin: 10px 0px;
+
+.table {
+  margin-bottom: 20px;
 }
 .page-container {
-  padding: 4px 0px;
-  text-align: right;
+  display: flex;
+  justify-content: flex-end;
 }
-.form-container {
-  padding: 0px 80px;
-}
+// .create-container {
+//   margin: 10px 0px;
+// }
+// .page-container {
+//   padding: 4px 0px;
+//   // text-align: right;
+//   // float: right;
+//   margin-left: 777px;
+// }
+// .form-container {
+//   padding: 0px 80px;
+// }
 </style>
